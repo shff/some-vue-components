b70d42d5c29d50530e86106cdd5d9f7f7d0767b0
Silvio Henrique Ferreira <shferreira@me.com>
2018-08-08 11:08:47 -0300

    Add Alert

--- lib/app/app.vue
+++ lib/app/app.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="pa4">
+    <Alert success>Alert!</Alert>
+    <Alert error>Alert!</Alert>
+    <Alert warning>Alert!</Alert>
+    <Alert info>Alert!</Alert>
   </div>
 </template>
 
---
+++ lib/components/alert.vue
@@ -0,0 +1,46 @@
+<template>
+  <div
+    :class="{
+      'bg-green': success,
+      'bg-red': error,
+      'bg-yellow': warning,
+      'bg-blue': !success && !warning && !error,
+    }"
+    class="sans-serif pa3 white b--white-40 bt bw2 mb3">
+    {{ message }}
+    <slot />
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Alert",
+    props: {
+      message: {
+        type: String,
+        required: false,
+        default: null,
+      },
+      info: {
+        type: Boolean,
+        default: false,
+        required: false,
+      },
+      success: {
+        type: Boolean,
+        default: false,
+        required: false,
+      },
+      error: {
+        type: Boolean,
+        default: false,
+        required: false,
+      },
+      warning: {
+        type: Boolean,
+        default: false,
+        required: false,
+      },
+    },
+  };
+</script>
